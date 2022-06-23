21e0de8472c1a41b2755628828cd8b0aedf96606
Вирстюк Михаил <m.vi@live.com>
2022-06-23 11:56:27 +0500

    Environment variables testing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var fn = @"c:\tmp\123.txt";
+string e = Environment.GetEnvironmentVariable("ENV_1") ?? "null";
+
+string fn = @"c:\tmp\123.txt";
 if (args.Length > 0)
     fn = args[0];
 
-while (true)
+do
 {
     Console.WriteLine(new FileInfo(fn).FullName + " - " + DateTime.Now.ToLongTimeString());
-    File.WriteAllText(fn, DateTime.Now.ToLongTimeString());
+    File.WriteAllText(fn, DateTime.Now.ToLongTimeString() + $"[{e}]");
     Thread.Sleep(3000);
 }
+while (args.Length < 2);
